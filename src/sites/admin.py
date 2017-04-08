5f59e14ec8a14cf19f2b3100a000fcf081e12f6b
lite.what <lite.what@gmail.com>
2017-04-09 00:12:05 +0200

    Added models test.

--- src/sites/admin.py
+++ src/sites/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Website, WebsiteCategory, WebPage
 
-# Register your models here.
+@admin.register(Website)
+class WebsiteAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(WebsiteCategory)
+class WebsiteCategoryAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(WebPage)
+class WebPageAdmin(admin.ModelAdmin):
+    pass
--- src/sites/tests.py
+++ src/sites/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
+from .models import WebPage, Website, WebsiteCategory
 
-# Create your tests here.
+
+class SavingNewObjectsTestCase(TestCase):
+
+    def setUp(self):
+        self.category = WebsiteCategory.objects.create(
+            name='searching',
+            description='Searching sites.',
+            count=0,
+        )
+        self.website = Website.objects.create(
+            url='www.google.com',
+            title='Google',
+            meta_description='Google searching.',
+            alexa_rank=1,
+            category=self.category,
+        )
+        self.page = WebPage.objects.create(
+            title='google',
+            website=self.website,
+            url='https://www.google.com/doodles',
+            meta_description='Google doodles',
+        )
+
+    def test_web_page_properly_saved(self):
+        page = WebPage.objects.get(title='google')
+        self.assertEqual(page, self.page)
+
+    def test_website_is_saved(self):
+        google = Website.objects.get(url='www.google.com')
+        self.assertEqual(
+            google.title,
+            'Google',
+            'google\'s title does not equals \'Google\''
+        )
+        self.assertEqual(google.category.name, 'searching')
